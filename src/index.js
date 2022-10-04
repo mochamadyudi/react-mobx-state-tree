7dac7250fed06bfec84a7d6bb4be89b208d60ece
mochamadyudi <myudisobari12@gmail.com>
2022-10-05 03:09:02 +0700

    updates'

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import promiseFinally from "promise.prototype.finally";
+import React from "react";
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(<App />,document.getElementById('root')
-);
+import { Provider } from "mobx-react";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const stores = {}
+window._____APP_STATE_____ = stores;
+
+promiseFinally.shim();
+'use strict'
+
+ReactDOM.render(
+    <Provider {...stores}>
+        <span>testing</span>
+    </Provider>,
+        document.getElementById("root")
+)
--- src/utils/index.js
+++
@@ -1,8 +0,0 @@
-class Utils {
-    static FormatNumber(number = 0, thousand = true,prefix = "$",prefixNumber = false){
-        return '$1.000.000'
-    }
-}
-
-
-export default Utils
